271fe6af1a8e5cb7eaefc6484319a9565d74b1e2
shafirpl <shafi.rpl@gmail.com>
2018-02-02 22:06:47 -0800

    Bootstrap complete

---
+++ Basic JavaScrpit Projects/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("First Name");
+var userLastName = prompt("Last Name");
+var age = prompt("Your age");
+console.log("Your Full Name is: "+userFirstName+" "+userLastName);
+console.log("Your age is: "+age);
