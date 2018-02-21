4b08f8fc645d8f179acb22b3da45752e9b2d8c13
shafirpl <shafi.rpl@gmail.com>
2018-02-21 02:56:50 -0800

    Advanced Dom Manipulation Complete

--- Basic DOM/Score_Keeper/Score_Keeper.js
+++ Basic DOM/Score_Keeper/Score_Keeper.js
@@ -7,12 +7,17 @@ var first = document.querySelector("#first");
 var second = document.querySelector("#second");
 var gameOver = false;
 var winningScore = 5;
+var resetButton = document.querySelector("#reset");
+var input = document.querySelector("input");
+var paragraph = document.querySelector("p span");
 
 p1Button.addEventListener("click",function(){
   if(!gameOver){
     p1Score++;
-    if(p1Score===winningScore)
+    if(p1Score===winningScore){
       gameOver = true;
+      first.classList.add("winner");
+    }
     first.textContent = p1Score;
   }
 });
@@ -20,8 +25,39 @@ p1Button.addEventListener("click",function(){
 p2Button.addEventListener("click",function(){
   if(!gameOver){
     p2Score++;
-    if(p2Score===winningScore)
+    if(p2Score===winningScore){
       gameOver = true;
+      second.classList.add("winner");
+    }
     second.textContent = p2Score;
   }
 });
+
+resetButton.addEventListener("click",function(){
+  //alert("clicked");
+  p1Score = 0;
+  p2Score = 0;
+  first.textContent = 0;
+  second.textContent = 0;
+  first.classList.remove("winner");
+  second.classList.remove("winner");
+  gameOver = false;
+});
+
+input.addEventListener("change",function(){
+  //alert("change the input");
+  paragraph.textContent = Number(this.value);
+  winningScore = Number(this.value);
+  reset();
+
+});
+
+function reset(){
+  p1Score = 0;
+  p2Score = 0;
+  first.textContent = 0;
+  second.textContent = 0;
+  first.classList.remove("winner");
+  second.classList.remove("winner");
+  gameOver = false;
+}
