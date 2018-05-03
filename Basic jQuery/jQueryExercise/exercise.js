7a64a21c72ad2273c23ee908b82c1ee4ab5457d4
shafirpl <shafi.rpl@gmail.com>
2018-05-03 01:57:43 -0700

    Basic jQuery Done

---
+++ Basic jQuery/jQueryExercise/exercise.js
@@ -0,0 +1,6 @@
+//alert("helo");
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+$("#third").css("border","orange");
+$("div").first().css("color","pink");
+$("div").get(1).css("color","red");
